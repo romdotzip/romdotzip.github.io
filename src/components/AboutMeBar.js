3affc866fbacd962f1a8d3bacafdfc589bec79e6
rm0819 <mattbsmith03@gmail.com>
2024-09-15 08:38:33 -0600

    Updated dropdown and made navbar sticky

--- src/components/AboutMeBar.js
+++ src/components/AboutMeBar.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function AboutMeBar() {
     return (
-        <div className={`aboutPage aboutNav`}>
+        <div className={`aboutPage aboutNav stickyaboutnav`}>
             <nav className="navbar">
                 <Link to="/videos" className="navbar">Videos</Link>
             </nav>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,18 @@
 import { Link } from 'react-router-dom';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function Navbar() {
     return (
-        <div className="navbar" id="navbar">
+        <div className="navbar stickynav" id="navbar">
           <nav className="navbar">
             <Link to="/home" className="navbar">Homepage</Link>
             |
-            <DropdownButton className="dropdown" id="dropdown-basic-button" title="Projects">
+            <div className="dropdown">
+              <button className="dropdown">Projects</button>
               <div className="dropdown-content">
-                <Dropdown.Item className="dropdowns" href="#/dndsearch">DnDSearch</Dropdown.Item>
-                <Dropdown.Item className="dropdowns" href="#/bomberman">Bomberman</Dropdown.Item>
+                <Link className="dropdown" to="/dndsearch">DnDSearch</Link>
+                <Link className="dropdown" to="/bomberman">Bomberman</Link>
               </div>
-            </DropdownButton>
+            </div>
             |
             <Link to="/about" className="navbar">About</Link>
           </nav>
