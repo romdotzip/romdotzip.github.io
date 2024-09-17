5c4be610ca6f2a3783e903e6dd02467b3d667ab5
rm0819 <mattbsmith03@gmail.com>
2024-09-17 14:30:09 -0600

    Updated dnd search with data card

--- src/pages/DndSearch.js
+++ src/pages/DndSearch.js
@@ -1,81 +1,108 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-var searchData = null;
-
-function Data() {
+function MonsterCard({data}) {
     return (
-        <p>Data</p>
-    )
+        <div className="dataCard">
+            <img 
+                src={"https://www.dnd5eapi.co" + data.image} 
+                alt={"Art of " + data.name}
+            />
+            <div className="dataInfo">
+                <p className="title">{data.name}</p>
+                <p><strong>HP:</strong> {data.hit_points}</p>
+                <p><strong>Type:</strong> {data.type}</p>
+                <p><strong>Size:</strong> {data.size}</p>
+                <p><strong>Alignment:</strong> {data.alignment}</p>
+                <p><strong>Languages:</strong> {data.languages}</p>
+                <p><strong>Strength:</strong> {data.strength}</p>
+            </div>
+        </div>
+    )    
 }
 
-function Search(e, type) {
-    const result = document.getElementById("result");
-    const form = e.target;
-    const formData = new FormData(form);
-    const formJson = Object.fromEntries(formData.entries());
-    const input = formJson.searchInput;
-    var desc = "";
-    result.innerHTML = "Searching...";
-
-    axios
-        .get(`https://www.dnd5eapi.co/api/` + type)
-        .then((response) => {
-            const monsters = response.data.results;
-            const matchedMonster = monsters.find(
-              (monster) => monster.name.toLowerCase() === input
-            );
-            if (matchedMonster) {
-              axios
-                .get(`https://www.dnd5eapi.co` + matchedMonster.url)
-                .then((monsterResponse) => {
-                    const monsterData = monsterResponse.data;
-                    searchData = monsterData;
-                    if (type === "monsters") {
-                        if (monsterData.desc) {
-
-                        } else {
-                            result.innerHTML = `
-                            <p>${monsterData.name}, ${monsterData.desc}, ${monsterData.image}</p>
-                            `;
-                        }
-                    } else {
-                        result.innerHTML = `
-                        <p>${monsterData.name}, ${monsterData.desc}, ${monsterData.level}, ${monsterData.range}, ${monsterData.image}</p>
-                        `;
-                    }
-                })
-                .catch((error) => {
-                    result.innerHTML = "Error fetching " + type + " details.";
-                });
-            } else {
-                if (type === "monsters") {
-                    result.innerHTML = "Monster not found.";
-                }
-                else {
-                    result.innerHTML = "Spell not found.";
-                }
-            }
-          })
-          .catch((error) => {
-            result.innerHTML = "Error fetching " + type + ".";
-        });
+function SpellCard({data}) {
+    return (
+        <div className="dataCard">
+            
+        </div>
+    )    
 }
 
-
-
+function DataCard({data, type}) {
+    if (JSON.stringify(data) === '{}') {
+        return (
+            <></>
+        )
+    }
+    if (type === "monsters") {
+        return <MonsterCard data={data}/>
+    } else if (type === "spells") {
+        return <SpellCard data={data}/>
+    }
+    
+}
 
 function DndSearch() {
     const[search, setSearch] = useState("monsters"); 
-    const[header, setHeader] = useState("Searching for monsters...");
-    const[displayData, setDisplayData] = useState(false);
+    const[header, setHeader] = useState("Searching for Monsters...");
+    const[data, setData] = useState("");
 
-    function ChoiceButton({type, children}) {
-        return (
-            <button onClick={() => { setSearch(type); setHeader("Searching for " + type + "...")}}>
-                {children}
-            </button>
-        )
+    async function Search(e, type) {
+        const result = document.getElementById("result");
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        const input = formJson.searchInput;
+        result.innerHTML = "Searching...";
+    
+        // See if monster exists
+        const monster = await axios
+            .get(`https://www.dnd5eapi.co/api/` + type, {
+                headers: {
+                    "Accept":"application/json"
+                }
+            })
+            .then((response) => {
+                const monsters = response.data.results;
+                const matchedMonster = monsters.find(
+                  (monster) => monster.name.toLowerCase() === input.toLowerCase()
+                );
+                if (matchedMonster) {
+                    result.innerHTML = "Found!";
+                    return matchedMonster;
+                } else {
+                    return false;
+                }
+            })
+            .catch((error) => {
+                result.innerHTML = "Error fetching " + type + ".";
+        });
+    
+        // If the monster doesnt exist:
+        if (monster === false) {
+            if (type === "monsters") {
+                result.innerHTML = "Monster not found.";
+            }
+            else {
+                result.innerHTML = "Spell not found.";
+            }
+            setData({});
+            return false;
+        }
+    
+        // Get specific monster data
+        const data = await axios
+            .get(`https://www.dnd5eapi.co` + monster.url)
+            .then((monsterResponse) => {
+                const monsterData = monsterResponse.data;
+                return monsterData;
+            })
+            .catch((error) => {
+                result.innerHTML = "Error fetching " + type + " details.";
+        });
+        setData(data);
+        return true;
     }
 
     return (
@@ -85,12 +112,16 @@ function DndSearch() {
             <title>Monster Search</title>
             <h1>D&D Information</h1>
             <div className="choiceButtons">
-                <ChoiceButton type="monsters">Search for monsters</ChoiceButton>
-                <ChoiceButton type="spells">Search for spells</ChoiceButton>
+                <button onClick={() => {setSearch("monsters"); setHeader("Searching for Monsters...")}}>
+                    Search for monsters
+                </button>
+                <button onClick={() => {setSearch("spells"); setHeader("Searching for Spells...")}}>
+                    Search for spells
+                </button>
                 <h2>{header}</h2>
             </div>
             <br />
-            <form onSubmit={e => {e.preventDefault(); setDisplayData(Search(e, search));}}>
+            <form onSubmit={e => {e.preventDefault(); Search(e, search);}}>
                 <input
                     name="searchInput"
                     id="monsterInput"
@@ -100,7 +131,10 @@ function DndSearch() {
             </form>
             <div id="result" className="result" />
             <div className="data">
-                {displayData && <Data/>}
+                <DataCard 
+                    data={data} 
+                    type={search}
+                />
             </div>
         </div>
     )
